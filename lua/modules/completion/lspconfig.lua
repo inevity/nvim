182de01700124b8075aa508a45af0a3ee12614d1
baul <roidinev@gmail.com>
2022-01-01 09:28:30 +0000

    lspconfig cap config and rust config

--- lua/modules/completion/lspconfig.lua
+++ lua/modules/completion/lspconfig.lua
@@ -16,8 +16,14 @@ saga.init_lsp_saga({
   max_preview_lines = 20,
 })
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 function _G.reload_lsp()
   vim.lsp.stop_client(vim.lsp.get_active_clients())
@@ -116,9 +122,45 @@ lspconfig.clangd.setup {
   },
 }
 -- rust analyzer :Done
-lspconfig.rust_analyzer.setup {
-  capabilities = capabilities,
+-- lspconfig.rust_analyzer.setup {
+  	
+--   capabilities = capabilities,
+-- }
+
+local opts = {
+    tools = {
+        autoSetHints = true,
+        hover_with_actions = true,
+        runnables = {
+            use_telescope = true
+        },
+        inlay_hints = {
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = "",
+        },
+    },
+
+    -- all the opts to send to nvim-lspconfig
+    -- these override the defaults set by rust-tools.nvim
+    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+    server = {
+        -- on_attach is a callback called when the language server attachs to the buffer
+        -- on_attach = on_attach,
+        settings = {
+            -- to enable rust-analyzer settings visit:
+            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+            ["rust-analyzer"] = {
+                -- enable clippy on save
+                checkOnSave = {
+                    command = "clippy"
+                },
+            }
+        }
+    },
 }
+
+require('rust-tools').setup(opts)
 
 -- dockerls, bashls, pyright
 local servers = {
