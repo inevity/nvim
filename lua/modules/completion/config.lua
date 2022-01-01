6a2bdf24cc14066b298f096d71ef06bf859043d2
baul <roidinev@gmail.com>
2022-01-01 13:47:03 +0000

    TODO: nvim-cmp custom

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -38,6 +38,7 @@ function config.nvim_cmp()
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
+    -- TODO: ref nvim-cmp advanced config or nvim-rust config 
     -- TODO: move to keymap init
     mapping = {
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
