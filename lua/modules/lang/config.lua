827534ae775e4aff3793ebeb22411264abb60954
baul <roidinev@gmail.com>
2022-05-22 11:38:08 +0800

    update nvim-treesitter config

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -5,8 +5,9 @@ function config.nvim_treesitter()
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require'nvim-treesitter.configs'.setup {
     -- list install
-    ensure_installed = "maintained",
+--     ensure_installed = "maintained",
     -- ensure_installed = { "bash", "c", "make", "cpp", "dart", "go", "gomod", "gowork", "java", "javascript", "jsdoc", "json", "json5", "kotlin", "llvm", "lua", "make" ,"markdown", "perl", "python", "regex", "rst", "rust", "toml", "typescript", "vim", "yaml" },
+    ensure_installed = {"bash", "c", "make", "cpp", "lua", "make" ,"markdown", "python", "rust"},
     -- Must PackerCompile
     sync_install = false,
     highlight = {
@@ -28,7 +29,7 @@ function config.nvim_treesitter()
 --   require("nvim-treesitter.install").prefer_git = true
     require("nvim-treesitter.install").command_extra_args = {
 --           curl = { "--proxy", "https://192.168.1.112:1088" },
-          curl = { "--proxy", "http://192.168.1.112:8080" },
+          curl = { "--proxy", "http://192.168.1.188:8080" },
     }
 end
 
